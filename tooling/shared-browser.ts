be1b638b6b9f76879d512f4159702f590294b247
Éric NICOLAS <ccjmne@gmail.com>
2026-03-26 16:40:12 +0100

    Switch back to using the economical puppeteer-core

--- tooling/shared-browser.ts
+++ tooling/shared-browser.ts
@@ -1,12 +1,12 @@
-import type { Browser } from 'puppeteer'
+import type { Browser } from 'puppeteer-core'
 import process from 'node:process'
-import puppeteer from 'puppeteer'
+import puppeteer from 'puppeteer-core'
 import { BehaviorSubject, firstValueFrom, from, Observable, of, Subject } from 'rxjs'
 import { delay, distinctUntilChanged, filter, shareReplay, switchMap, tap, withLatestFrom } from 'rxjs/operators'
 import which from 'which'
 
 const keepalive = +(process.env.BROWSER_KEEPALIVE || '30000')
-const executablePath = process.env.BROWSER_EXECUTABLE || [puppeteer.executablePath(), 'chromium', 'chromium-browser', 'google-chrome-stable', 'google-chrome']
+const executablePath = process.env.BROWSER_EXECUTABLE || ['chromium', 'chromium-browser', 'google-chrome-stable', 'google-chrome']
   .map(name => which.sync(name, { nothrow: true }))
   .find(ex => !!ex)
 if (!executablePath) throw new Error('No suitable browser executable found.')
